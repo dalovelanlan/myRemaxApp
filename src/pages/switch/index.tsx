401aa079cc4816736508ba471ec3b9f87382b80f
dangh <dangh>
2021-10-28 17:53:58 +0800

    开关按钮

--- src/pages/switch/index.tsx
+++ src/pages/switch/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import {View} from 'remax/one'
-
+import React, { useState } from 'react'
+import { View, Text } from 'remax/one'
+import { Switch,GenericEvent } from 'remax/wechat'
+import './index.css'
 
 const Index = () => {
-    return (
-        <View>
-            Switch
-        </View>
-    )
+  const [isChecked, setIsChecked] = useState(false)
+  const [status, setStatus] = useState('关')
+
+  // 点击switch开关灯
+  const handleChangeSwitch = (event: GenericEvent) => { 
+    setIsChecked(event.detail.value)
+    setStatus(event.detail.value?'开':'关')
+  }  
+
+  return (
+    <View className='pageContainer'>
+      <View className='page_switch_body'>
+        <Switch
+          color='#1f4f89'
+          onChange={handleChangeSwitch}
+          checked={isChecked}
+        />
+        <Text className='page_switch_status'>{status}</Text>
+      </View>
+    </View>
+  )
 }
 
 export default Index
